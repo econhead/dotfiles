9294a526a4bd02feb48c297697aab44a9caf1e8d
Laxman <laxmanthakur5233@gmail.com>
2025-03-21 15:07:47 +0530

    Changed font settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,9 +9,9 @@ config.window_background_opacity = 0.7
 config.macos_window_background_blur = 0
 config.prefer_egl = true
 config.window_padding = {
-	left = 5,
+	left = 0,
 	right = 0,
-	top = 10,
+	top = 0,
 	bottom = 0,
 }
 config.automatically_reload_config = true
