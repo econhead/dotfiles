8f996e780214a39e03d74dc78f786267e9fa4692
Laxman <laxmanthakur5233@gmail.com>
2026-09-21 10:03:16 +0530

    Added noctalia to install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Ensure necessary directories exist
-mkdir -p ~/.config
+mkdir -p ~/.config ~/.local/state/noctalia
 
 # Symlink dotfiles
-# ln -sfn ~/.dotfiles/aerospace ~/.config/aerospace
-# ln -sfn ~/.dotfiles/wezterm ~/.config/wezterm
+ln -sfn ~/.dotfiles/aerospace ~/.config/aerospace
+ln -sfn ~/.dotfiles/wezterm ~/.config/wezterm
 ln -sfn ~/.dotfiles/zathura ~/.config/zathura
-# ln -sfn ~/.dotfiles/starship ~/.config/starship
-# ln -sfn ~/.dotfiles/karabiner ~/.config/karabiner
+ln -sfn ~/.dotfiles/starship ~/.config/starship
+ln -sfn ~/.dotfiles/karabiner ~/.config/karabiner
 ln -sfn ~/.dotfiles/tmux ~/.config/tmux
 ln -sfn ~/.dotfiles/nvim ~/.config/nvim
-# ln -sfn ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sfn ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sfn ~/.dotfiles/ghostty ~/.config/ghostty
 ln -sfn ~/.dotfiles/kitty ~/.config/kitty
 ln -sfn ~/.dotfiles/hypr ~/.config/hypr
@@ -19,4 +19,7 @@ ln -sfn ~/.dotfiles/fastfetch ~/.config/fastfetch
 ln -sfn ~/.dotfiles/sioyek ~/.config/sioyek
 ln -sfn ~/.dotfiles/xremap ~/.config/xremap
 ln -sfn ~/.dotfiles/bash/.bashrc ~/.bashrc
+ln -sfn ~/.dotfiles/noctalia/settings.toml ~/.local/state/noctalia/settings.toml
+
+
 echo "Dotfiles setup complete!"
